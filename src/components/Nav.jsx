64cac1b0230f62051fd674f90008ccbbb0252e55
Bogdan Caliman <calimanbf@gmail.com>
2023-09-04 21:49:16 +0300

    all components created

---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+const Nav = () => {
+  return <div>Nav TEST</div>;
+};
+
+export default Nav;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Service from "./Service";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Service,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
