ac973733b0fe522f205f09163610c08c14d64817
Bogdan Caliman <calimanbf@gmail.com>
2023-09-05 23:11:21 +0300

    adding basic elemnts to the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,26 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
   return (
-    <section id="home" className="w-full border-2 border-red-500 p-2">
-      <h1>Hero</h1>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+      </div>
     </section>
   );
 };
