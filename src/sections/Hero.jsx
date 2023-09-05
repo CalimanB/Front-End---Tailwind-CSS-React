8e6c81d3cd2d6a45651600031d4feee7b12bc5f6
Bogdan Caliman <calimanbf@gmail.com>
2023-09-05 22:56:43 +0300

    started working on the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,9 @@
 const Hero = () => {
-  return <div>TEST</div>;
+  return (
+    <section id="home" className="w-full border-2 border-red-500 p-2">
+      <h1>Hero</h1>
+    </section>
+  );
 };
 
 export default Hero;
