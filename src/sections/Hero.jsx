11e1b15d5e05d6b9ccef131366e5d7a678af59ce
Bogdan Caliman <calimanbf@gmail.com>
2023-09-07 21:16:33 +0300

    main flex debugged

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer Collections
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
@@ -25,16 +25,17 @@ const Hero = () => {
           your active life.
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
-      </div>
-      <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
-        {statistics.map((stat) => (
-          <div key={stat.label}>
-            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-            <p className="leading-7 font-montserrat text-slate-gray">
-              {stat.label}
-            </p>
-          </div>
-        ))}
+
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
