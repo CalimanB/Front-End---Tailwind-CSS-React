e9565085f0d7abbd995027b471c7815e1245a8b3
Bogdan Caliman <calimanbf@gmail.com>
2023-09-15 21:11:43 +0300

    Text and button styled

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -15,15 +15,20 @@ const SuperQuality = () => {
             The New Arrival
           </span>
           <br /> */}
-          We Provide You
-          <span className="text-coral-red">Supre</span> Shoes
+          We Provide You <span className="text-coral-red">Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
         </h2>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
-
-        <Button label="View details" />
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
       </div>
     </section>
   );
